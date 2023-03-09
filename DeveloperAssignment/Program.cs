92865591043881c8c5e09fce01cf40b21843992c
rasalimi99 <127392070+rasalimi99@users.noreply.github.com>
2023-03-09 02:49:46 -0700

    Add files via upload

---
+++ DeveloperAssignment/Program.cs
@@ -0,0 +1,62 @@
+using DeveloperAssignment.Application;
+using DeveloperAssignment.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add<ParseBadRequest>();
+}).ConfigureApiBehaviorOptions(BadRequestsBehavior.Parse);
+
+builder.Services.AddTransient<IGeneral, General>();
+builder.Services.AddTransient<IEntities, Entities>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+
+            //you can configure your custom policy
+            builder.AllowAnyOrigin()
+                                .AllowAnyHeader()
+                                .AllowAnyMethod().WithExposedHeaders(new string[] { "totalAmountOfRecords" });
+        });
+});
+
+
+builder.Services.AddAutoMapper(Assembly.Load("DeveloperAssignment.Infrastructure"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
